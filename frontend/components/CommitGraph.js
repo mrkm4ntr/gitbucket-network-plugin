762aac1ac9c99b6056f0a27d88adc753fd323ff0
Shintaro Murakami <mrkm4ntr@gmail.com>
2016-06-04 23:27:23 +0900

    Adjust size of svg

--- frontend/components/CommitGraph.js
+++ frontend/components/CommitGraph.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Commit from './Commit';
 
 const CommitGraph = ({ maxLane, commits }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="2000" width="800">
+  <svg xmlns="http://www.w3.org/2000/svg" height={commits.length * 30} style={{width: '100%'}}>
     <defs>
       <marker id="arr" markerUnits="strokeWidth" markerWidth="3" markerHeight="3" viewBox="0 0 10 10" refX="5" refY="5" orient="auto">
         <polygon points="0,0 5,5 0,10 10,5 " fill="red"/>
