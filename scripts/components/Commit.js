bc4c8ca19bb8dfce19a85465e96ae76c8ad0ad07
Shintaro Murakami <mrkm4ntr@gmail.com>
2016-06-07 00:09:04 +0900

    Different colors for every lane

--- scripts/components/Commit.js
+++ scripts/components/Commit.js
@@ -1,26 +1,31 @@
 import React, { PropTypes } from 'react';
 
 const height = 30;
+const pallet = ['#000000', '#FFD700', '#C71585', '#006400', '#0000ff'];
 
-const Commit = ({ maxLane, index, lane, message, parents, id, avatarUrl }) => (
-  <g transform={`translate(0, ${index * height})`}>
+const Commit = ({ maxLane, index, lane, message, parents, id, avatarUrl }) => {
+
+  const x = (maxLane + 2) * 15;
+  const color = pallet[lane % pallet.length];
+
+  return <g transform={`translate(0, ${index * height})`}>
     {parents.map(p => {
       if (lane === p.lane) {
-        return <path key={p.index} fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 v${(p.index - index) * height}`} stroke="#000000"></path>
+        return <path key={p.index} fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 v${(p.index - index) * height}`} stroke={color}></path>
       }
       if (lane < p.lane) {
-        return <path key={p.index} fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 l5,5 h${(p.lane - lane) * 15 - 5} v${(p.index - index) * height - 5}`} stroke="#000000" markerEnd={'url(#arr)'}></path>
+        return <path key={p.index} fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 l5,5 h${(p.lane - lane) * 15 - 5} v${(p.index - index) * height - 5}`} stroke={pallet[p.lane % pallet.length]}></path>
       }
-      return <path key={p.index} marker-end="url(#arr)" fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 v${(p.index - index) * height} h${(p.lane - lane) * 15}`} stroke="#000000"></path>
+      return <path key={p.index} fill="none" stroke-width="2" d={`M${lane * 15 + 10},15 v${(p.index - index) * height} h${(p.lane - lane) * 15}`} stroke={color}></path>
     })}
-    <rect x={maxLane * 30} y="5" width="20" height="20" stroke="#a3c5cc" fill="none" stroke-width="2"></rect>
-    <circle cx={lane * 15 + 10} cy="15" r="3" onClick={() => {location.href = `./commit/${id}`}}></circle>
-    <image x={maxLane * 30} y="5" width="20" height="20" preserveAspectRatio="none" href={avatarUrl} ></image>
-    <text x={maxLane * 30 + 30} y="15" width="20" height="20" fill="#000000" stroke-width="2" font="14px">
+    <rect x={x} y="5" width="20" height="20" stroke={color} fill="none" stroke-width="2"></rect>
+    <circle cx={lane * 15 + 10} cy="15" r="3" stroke={color} fill={color} onClick={() => {location.href = `./commit/${id}`}}></circle>
+    <image x={x} y="5" width="20" height="20" preserveAspectRatio="none" href={avatarUrl} ></image>
+    <text x={x + 30} y="15" width="20" height="20" fill="#000000" stroke-width="2" font="14px">
       <tspan dy="5">{message}</tspan>
     </text>
   </g>
-);
+};
 
 Commit.propTypes = {
   maxLane: PropTypes.number.isRequired,
