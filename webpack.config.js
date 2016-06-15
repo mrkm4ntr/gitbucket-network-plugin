d504ee9f8af3215c252a52b7482a23761dd25d1a
Shintaro Murakami <mrkm4ntr@gmail.com>
2016-06-15 18:29:27 +0900

    Transform Object.assign

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel', // 'babel-loader' is also a legal name to reference
         query: {
-          presets: ['stage-0', 'es2015', 'react']
+          presets: ['stage-0', 'es2015', 'react'],
+          plugins: ['transform-object-assign']
         }
       }
     ]
