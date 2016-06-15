ef92d584a6bd3469a54c83048bd71b49bea306b7
Shintaro Murakami <mrkm4ntr@gmail.com>
2016-06-15 12:54:46 +0900

    Reduce size of script

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap'
+import DropdownButton from 'react-bootstrap/lib/DropdownButton'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
 import CommitGraph from './CommitGraph';
 
 export default class App extends React.Component {
